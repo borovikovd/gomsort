095e0e32d76c69135e9e3d63ac42b515ee5d093b
Denis Borovikov <denis.borovikov@gmail.com>
2025-08-04 12:52:14 +0100

    Fix test

--- cmd/msort_test.go
+++ cmd/msort_test.go
@@ -52,6 +52,15 @@ func TestRunWithActualSort(t *testing.T) {
 	tmpDir := t.TempDir()
 	testFile := filepath.Join(tmpDir, "test.go")
 
+	// Create go.mod file
+	goModContent := `module testmodule
+
+go 1.22
+`
+	if err := os.WriteFile(filepath.Join(tmpDir, "go.mod"), []byte(goModContent), 0644); err != nil {
+		t.Fatal(err)
+	}
+
 	testContent := `package test
 
 type Server struct{}
