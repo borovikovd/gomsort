1bb3046977faee09a847c2ebf618e9a2b28ea555
Denis Borovikov <denis.borovikov@gmail.com>
2025-08-04 13:17:24 +0100

    Reformat code

--- pkg/analyzer/testdata/src/a/a.go
+++ pkg/analyzer/testdata/src/a/a.go
@@ -10,5 +10,6 @@ func (s *Server) Start() error {
 func (s *Server) helper() string {
 	return "help"
 }
+
 // want "methods in this file could be better sorted for readability"
 // Methods are in wrong order - helper before entry points
